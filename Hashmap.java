42719d21b8c7f9a4019ebbf3b8a95ffbf2bd6e69
leandrodalvi <leandrolopesdalvi@gmail.com>
2020-06-28 19:26:31 -0300

    Update Hashmap.java

--- Hashmap.java
+++ Hashmap.java
@@ -1,5 +1,50 @@
 package TrabalhoPO;
 
 public class Hashmap{
+    private List[] array;
     
+    public Hashmap(int n){
+        // n equals number of elements
+        int m = n + 1;
+        boolean canStop = false;
+        while(!canStop){
+            if(IsPrime(m)){
+                canStop = true;
+            }else{
+                m++;
+            }
+        }
+        this.array = new List[m];
+
+    }
+
+    private boolean IsPrime(int n){
+        int i;
+        if(n < 2){
+            return false;
+        }
+
+        for(i = 2; i < n; i++){
+            if(i % n == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int hashing(int key){
+        return (key%this.array.length);
+    }
+
+    public void insert(Item novo){
+        int key = hashing(novo.getCpf());
+        if(array[key] != null){
+            array[key] = new List();
+            array[key].insere_final(novo);
+        }else{
+            array[key].insere_final(novo);
+        }
+    }
+
 }
