bb8e031175b18d201270026bea0db1073689b71e
leandrodalvi <leandrolopesdalvi@gmail.com>
2020-06-30 18:29:56 -0300

    Update

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
 package TrabalhoPO;
+import java.io.*;
 
 public class Main{
-    
+    public static void main(){
+        try{
+            FileWriter writer = new FileWriter("HeapAle.txt");
+            writer.write("");
+        }catch(IOException e){
+            System.out.println("Error with File Writing");
+        }
+
+    }
 }
